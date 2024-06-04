fe0af874ac001a21550213fd900d95628d28313c
z-jxy <zjxy.00@gmail.com>
2024-06-04 11:10:10 -0400

    updates

--- src/analyzer/mod.rs
+++ src/analyzer/mod.rs
@@ -5,6 +5,8 @@ use realfft::RealFftPlanner;
 
 use crate::midi;
 
+const FRAME_SIZE: usize = 2048 * 8;
+
 pub fn detect_note_switches(reader: WavReader<BufReader<File>>) -> Vec<String> {
     let spec = reader.spec();
     let samples: Vec<f32> = reader
@@ -12,36 +14,34 @@ pub fn detect_note_switches(reader: WavReader<BufReader<File>>) -> Vec<String> {
         .map(|s| s.unwrap() as f32 / i16::MAX as f32)
         .collect();
 
-    let frame_size = 2048 * 8; // Adjust frame size as needed
-    let hop_size = frame_size / 4; // 50% overlap
+    // let frame_size = 2048 * 8; // Adjust frame size as needed
+    let hop_size = FRAME_SIZE / 4; // 50% overlap
     let sample_rate = spec.sample_rate as f32;
-    let bin_freq = sample_rate / frame_size as f32;
+    let bin_freq = sample_rate / FRAME_SIZE as f32;
     let bass_threshold = 80.0; // Adjust threshold as needed
-                               // let debounce_frames = 10; // Number of frames to debounce repeated note detection
 
     // Set up FFT with the correct frame size
     let mut planner = RealFftPlanner::<f32>::new();
-    let fft = planner.plan_fft_forward(frame_size);
+    let fft = planner.plan_fft_forward(FRAME_SIZE);
 
     let mut prev_note = String::new();
     let mut output = fft.make_output_vec();
-    // let mut last_detection_frame = 0;
     let mut notes = vec![];
     let mut detected_notes = Vec::new();
     let debounce_frames = 10; // Number of frames to debounce repeated note detection
 
     for i in (0..samples.len()).step_by(hop_size) {
-        if i + frame_size >= samples.len() {
+        if i + FRAME_SIZE >= samples.len() {
             break;
         }
 
-        let frame = &samples[i..i + frame_size];
+        let frame = &samples[i..i + FRAME_SIZE];
 
         // Apply a Hann window to the frame
         let mut windowed_frame: Vec<f32> = frame
             .iter()
             .enumerate()
-            .map(|(i, &x)| x * 0.5 * (1.0 - (2.0 * PI * i as f32 / (frame_size - 1) as f32).cos()))
+            .map(|(i, &x)| x * 0.5 * (1.0 - (2.0 * PI * i as f32 / (FRAME_SIZE - 1) as f32).cos()))
             .collect();
 
         // Perform FFT
@@ -51,21 +51,6 @@ pub fn detect_note_switches(reader: WavReader<BufReader<File>>) -> Vec<String> {
 
         // Calculate magnitudes
         let magnitudes: Vec<f32> = output.iter().map(|c| c.norm()).collect();
-
-        // Analyze lower frequencies to find the bass root note
-        // let mut max_magnitude = 0.0;
-        // let mut bass_freq = 0.0;
-
-        // for (j, &magnitude) in magnitudes.iter().enumerate() {
-        //     let freq = j as f32 * bin_freq;
-        //     if freq > bass_threshold {
-        //         break;
-        //     }
-        //     if magnitude > max_magnitude {
-        //         max_magnitude = magnitude;
-        //         bass_freq = freq;
-        //     }
-        // }
 
         if let Some(bass_freq) = pick_peak_frequency(&magnitudes, bin_freq, bass_threshold) {
             let midi_note = midi::frequency_to_midi(bass_freq);
